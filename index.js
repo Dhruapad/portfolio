03f9d7923da9e61a63bd590c565ec49538a3a1e1
Dhruapad <115549690+Dhruapad@users.noreply.github.com>
2023-06-08 21:10:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var options = {
+    strings: ['Dhrupad', 'UI Desiner',  'Web Developer' ,"Video Editor" ,"Game Developer"],
+    typeSpeed: 100,
+    backSpeed:100,
+    loop: true
+  };
+var typed = new Typed('.typing', options);
